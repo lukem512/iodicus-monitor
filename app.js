187a64c677317fd59579911ee7881f58e676cb92
lukem512 <lukem95@gmail.com>
2016-06-14 12:38:28 +0100

    Spring cleaning

--- app.js
+++ app.js
@@ -6,12 +6,22 @@ let TARGET_VALUE = process.env.TARGET_VALUE || 123;
 let GATEWAY_MAC_ID = process.env.GATEWAY_MAC_ID || 11223344556677;
 
 let SOCKET_WAIT_TIME = process.env.SOCKET_WAIT_TIME || 3000;
+let TEST_WAIT_TIME = process.env.TEST_WAIT_TIME || 15000;
+
+const EXIT_SUCCESS = 0, EXIT_FAILURE = 1;
+
+// Flags indicating the test results
+let test_ep_http_receive_success = false;   // EP HTTP responded correctly to request
+let test_ep_message_queue_ack = false;      // RabbitMQ ack'd request
+let test_ss_http_receive_success = false;   // HTTP update message received by SS
+let test_ss_ws_send_success = false;        // SS sent correct WS message
+let test_ep_message_queue_success = false;  // RabbitMQ provided correct messages
 
 // Default socket factory
 function connect_to_socket(name, host, port) {
   let socket_uri = host + ':' + port;
 
-  console.log('[SOCKET] Connecting to ' + name + ' at \'' + socket_uri + '\'');
+  console.info('[SOCKET] Connecting to ' + name + ' at \'' + socket_uri + '\'');
   return io.connect(socket_uri, { reconnect: true });
 };
 
@@ -20,23 +30,7 @@ var ebe_socket_connected, secure_server_socket_connected = false;
 
 function wait_for_sockets() {
   if (ebe_socket_connected && secure_server_socket_connected) {
-    console.log('[SOCKET] Calling requestUpdateDeviceData');
-
-    // Call the mock_ebe to make a device update
-    ebe_socket.emit('call', {
-      routineName: 'requestUpdateDeviceData',
-      args: {
-        device_parameter_id: DEVICE_PARAMETER_ID,
-        target_value: TARGET_VALUE
-      }
-    }, function(err, success) {
-      if (err) return console.error('[SOCKET] Error:', err);
-      console.log('[SOCKET] Resulting value was ' + success);
-
-      if (success) {
-        console.log('[MONITOR] REST API is online!');
-      }
-    });
+    test();
   } else {
     setTimeout(wait_for_sockets, SOCKET_WAIT_TIME);
   }
@@ -58,7 +52,7 @@ ebe_socket.on('connect', data => {
 
 // Subscribe to the receive event
 ebe_socket.on('receive', data => {
-  console.log('[SOCKET] Received \'receive\' event from ' + MOCK_EBE_SOCKET_NAME);
+  console.info('[SOCKET] Received \'receive\' event from ' + MOCK_EBE_SOCKET_NAME);
   let obj = JSON.parse(data);
 
   /* { current: 105,
@@ -75,6 +69,12 @@ ebe_socket.on('receive', data => {
       if (obj.intent.toString() == TARGET_VALUE.toString()) {
         console.log('[MONITOR] Change request acknowledged by Message Queue')
       }
+
+      if (obj.intent.toString() == obj.current.toString()) {
+        console.error('[MONITOR] No change will occur - the final Message Queue event will not be received');
+      }
+
+      test_ep_message_queue_ack = true;
     }
   }
 
@@ -90,21 +90,13 @@ ebe_socket.on('receive', data => {
   if (obj.action == 'change_record') {
     if (obj.device_parameter.toString() == DEVICE_PARAMETER_ID.toString()) {
       if (obj.current.toString() == TARGET_VALUE.toString()) {
-        console.log('[MONITOR] Message Queue is online!')
+        console.log('[MONITOR] Message Queue is online!');
+        test_ep_message_queue_success = true;
       }
     }
   }
 });
 ebe_socket.emit('subscribe', { eventName: 'receive' });
-
-// Subscribe to the send event
-ebe_socket.on('send', data => {
-  console.log('[SOCKET] Received \'send\' event from ' + MOCK_EBE_SOCKET_NAME);
-  let obj = JSON.parse(data);
-
-  console.log(obj)
-});
-ebe_socket.emit('subscribe', { eventName: 'send' });
 
 //
 // Mock Secure Server
@@ -122,7 +114,7 @@ ebe_socket.on('connect', data => {
 
 // Subscribe to the receive event
 secure_server_socket.on('receive', data => {
-  console.log('[SOCKET] Received \'receive\' event from ' + MOCK_SECURE_SERVER_SOCKET_NAME);
+  console.info('[SOCKET] Received \'receive\' event from ' + MOCK_SECURE_SERVER_SOCKET_NAME);
   let obj = JSON.parse(data);
 
   /* {
@@ -143,6 +135,7 @@ secure_server_socket.on('receive', data => {
     obj.DeviceData.DPDO.some(dpdo => {
       if (dpdo.CV == TARGET_VALUE) {
         console.log('[MONITOR] Secure Server HTTP interface is online!');
+        test_ss_http_receive_success = true;
         return true;
       }
       return false;
@@ -153,7 +146,7 @@ secure_server_socket.emit('subscribe', { eventName: 'receive' });
 
 // Subscribe to the send event
 secure_server_socket.on('send', data => {
-  console.log('[SOCKET] Received \'send\' event from ' + MOCK_SECURE_SERVER_SOCKET_NAME);
+  console.info('[SOCKET] Received \'send\' event from ' + MOCK_SECURE_SERVER_SOCKET_NAME);
   let obj = JSON.parse(data);
 
   /*
@@ -194,6 +187,7 @@ secure_server_socket.on('send', data => {
             if (dpdo.CV == TARGET_VALUE) {
               found = true;
               console.log('[MONITOR] Secure Server WebSocket interface is online!');
+              test_ss_ws_send_success = true;
             }
             return found;
           });
@@ -209,6 +203,77 @@ secure_server_socket.emit('subscribe', { eventName: 'send' });
 //
 // Main test methods
 //
+
+// Check the test for success
+function test_checker() {
+  let success = true;
+
+  console.log('\n*** RESULTS *** \n');
+
+  if (!test_ep_http_receive_success) {
+    console.error('[FAIL] The EP HTTP interface is not accessible');
+    success = false;
+  }
+
+  if (!test_ep_message_queue_ack) {
+    console.error('[FAIL] The Message Queue did not acknowledge the update request');
+    success = false;
+  }
+
+  if (!test_ss_http_receive_success) {
+    console.error('[FAIL] The Mock Secure Server did not receive an update message from the EP');
+    success = false;
+  }
+
+  if (!test_ss_ws_send_success) {
+    console.error('[FAIL] The Mock Secure Server did not send a WebSocket push notification');
+    success = false;
+  }
+
+  if (!test_ep_message_queue_success) {
+    console.error('[FAIL] The Message Queue did not report the update');
+
+    if (!test_ep_message_queue_ack) {
+      success = false;
+    } else {
+      console.warn('[WARN] Check that the TARGET_VALUE (\'' + TARGET_VALUE + '\') differs from the current value');
+    }
+  }
+
+  if (success) {
+    console.log('\nTests passed successfully!')
+    process.exit(EXIT_SUCCESS);
+  } else {
+    console.error('\nOne or more tests failed')
+    process.exit(EXIT_FAILURE);
+  }
+};
+
+// Perform the test
+function test() {
+  console.log('[MONITOR] Starting test...');
+  console.info('[SOCKET] Calling requestUpdateDeviceData');
+
+  // Call the mock_ebe to make a device update
+  ebe_socket.emit('call', {
+    routineName: 'requestUpdateDeviceData',
+    args: {
+      device_parameter_id: DEVICE_PARAMETER_ID,
+      target_value: TARGET_VALUE
+    }
+  }, function(err, success) {
+    if (err) return console.error('[SOCKET] Error:', err);
+    console.info('[SOCKET] Resulting value was ' + success);
+
+    if (success) {
+      console.log('[MONITOR] REST API is online!');
+      test_ep_http_receive_success = true;
+    }
+  });
+
+  // Start timeout
+  setTimeout(test_checker, TEST_WAIT_TIME);
+};
 
 // Spin until the sockets have connected
 setTimeout(wait_for_sockets, SOCKET_WAIT_TIME);
